f7c76bcda9ad4eda3002d41377015ac5253e82a5
wasinaphat <wasinapl@scg.com>
2021-05-02 15:19:40 +0700

    DAO Stucture

--- user-api/controllers/users_controller.go
+++ user-api/controllers/users_controller.go
@@ -2,17 +2,49 @@ package controllers
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-microservice/user-api/domain/users"
+	"github.com/golang-microservice/user-api/services"
+	"github.com/golang-microservice/user-api/utils/errors"
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented,"Create User")
+	var user users.User
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		// TODO : return bad request to the caller
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
+		// TODO: handle user creation error
+		return
+	}
+	c.JSON(http.StatusCreated, result)
 }
 func GetUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented,"Get User")
+	userId, userErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
+
+	if userErr != nil {
+		err := errors.NewBadRequestError("user id should be a number")
+		c.JSON(err.Status, err)
+		return
+	}
+	result,getErr := services.GetUser(userId)
+	if getErr!=nil{
+		c.JSON(getErr.Status,getErr)
+		return
+	}
+	c.JSON(http.StatusCreated, result)
 }
 
+
+
 func SearchUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented,"Search User")
+	c.String(http.StatusNotImplemented, "Search User")
 }
--- user-api/services/users_service.go
+++ user-api/services/users_service.go
@@ -1,1 +1,27 @@
 package services
+
+import (
+	"github.com/golang-microservice/user-api/domain/users"
+	"github.com/golang-microservice/user-api/utils/errors"
+)
+
+func GetUser(userId int64) (*users.User, *errors.RestErr) {
+	result := &users.User{Id: userId}
+
+	if err := result.Get(); err != nil {
+		return nil, err
+	}
+	return result, nil
+
+}
+
+func CreateUser(user users.User) (*users.User, *errors.RestErr) {
+	if err := user.Validate(); err != nil {
+		return nil, err
+	}
+	if err := user.Save(); err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
