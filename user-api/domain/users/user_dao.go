3081079e7c375ec50347f62ece454ff4ff0edde9
wasinaphat <wasinapl@scg.com>
2021-05-02 16:41:39 +0700

    add  query user

--- user-api/domain/users/user_dao.go
+++ user-api/domain/users/user_dao.go
@@ -11,26 +11,25 @@ import (
 
 const (
 	indexUniqueEmail = "email_UNIQUE"
+	errorNoRows      = "no rows in result set"
 	queryInsertUser  = "INSERT INTO users(first_name, last_name, email, date_created) VALUES(?,?,?,?);"
-)
-
-var (
-	usersDB = make(map[int64]*User)
+	queryGetUser     = "SELECT id, first_name, last_name, email, date_created FROM users WHERE id=?;"
 )
 
 func (user *User) Get() *errors.RestErr {
-	if err := users_db.Client.Ping(); err != nil {
-		panic(err)
+	stmt, err := users_db.Client.Prepare(queryGetUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
 	}
-	result := usersDB[user.Id]
-	if result == nil {
-		return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
+	defer stmt.Close()
+
+	result := stmt.QueryRow(user.Id)
+	if err := result.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated); err != nil {
+		if strings.Contains(err.Error(), errorNoRows) {
+			return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
+		}
+		return errors.NewInternalServerError(fmt.Sprintf("error when trying to get user %d: %s ", user.Id, err.Error()))
 	}
-	user.Id = result.Id
-	user.FirstName = result.FirstName
-	user.LastName = result.LastName
-	user.Email = result.Email
-	user.DateCreated = result.DateCreated
 	return nil
 }
 func (user *User) Save() *errors.RestErr {
@@ -53,6 +52,6 @@ func (user *User) Save() *errors.RestErr {
 		return errors.NewInternalServerError(fmt.Sprintf("error when trying to save user %s", err.Error()))
 	}
 	user.Id = userId
-//
+	//
 	return nil
 }
